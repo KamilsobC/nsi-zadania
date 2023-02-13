285cc42b23e82e6e9a84ac0f266136247015c7aa
Kamil Sobczak <kamil.sobczak@talentech.com>
2023-02-13 18:52:06 +0100

    zad 3 ready

--- mnit_classifier.py
+++ mnit_classifier.py
@@ -9,50 +9,46 @@ class NN():
         self.data_amount = data_amount
         self.test_amount = int(data_amount/4) 
         self.batch_size = batch_size
-        self.hidden_neurons = hidden_neurons
+        self.steps = data_amount // self.batch_size
+
         self.desired_epochs = epochs
         self.path = path
         self.learning_rate = learning_rate
+        self.hidden_neurons = hidden_neurons
 
-    def load_data(self):
+    def load_data(self,train=True):
         with open('training/data.json') as f:
             print('Loading data from json...')
             raw_data = json.load(f)
-
-        self.training_set = np.array(raw_data['images'])[:self.data_amount]
-        self.labels = np.array(raw_data['labels'])[:self.data_amount]
-        self.test_set = np.array(raw_data['images_test'])[:self.test_amount]
-        self.test_labels = np.array(raw_data['labels_test'])[:self.test_amount]
+        if train:
+            self.training_set = np.array(raw_data['images'])[:self.data_amount]
+            self.labels = np.array(raw_data['labels'])[:self.data_amount]
+        else:
+            self.test_set = np.array(raw_data['images_test'])[:self.test_amount]
+            self.test_labels = np.array(raw_data['labels_test'])[:self.test_amount]
 
     def _normalize_data_set(self,data):
         data = (data.astype(np.float32) - 127.5) / 127.5
         data =  data.reshape(data.shape[0], data.shape[1] * data.shape[2])
         return data
     
-    
-    def data_preprocessing(self):
-        self.training_set = self._normalize_data_set(self.training_set)
-        self.test_set = self._normalize_data_set(self.test_set)
 
-        # Data Shafelling
-        keys = np.array(range(self.training_set.shape[0]))
-        np.random.shuffle(keys)
+    def data_preprocessing(self,train=True):
+        if train:
+            self.training_set = self._normalize_data_set(self.training_set)
+            
+            #Shuffle
+            keys = np.array(range(self.training_set.shape[0]))
+            np.random.shuffle(keys)
 
+            self.training_set = self.training_set[keys]
+            self.labels = self.labels[keys]
 
-        self.training_set = self.training_set[keys]
-        self.labels = self.labels[keys]
-
-        # Labers preporcessing
-        self.processed_labels = np.zeros((len(self.training_set), 10))
-        for i in range(len(self.training_set)):
-            self.processed_labels[i][self.labels[i]] = 1
-        
-
-    def chunking(self):
-        self.steps = self.training_set.shape[0] // self.batch_size
-
-        if self.steps * self.batch_size < self.training_set.shape[0]:
-            self.steps += 1
+            self.processed_labels = np.zeros((len(self.training_set), 10))
+            for i in range(len(self.training_set)):
+                self.processed_labels[i][self.labels[i]] = 1
+        else:
+            self.test_set = self._normalize_data_set(self.test_set)
 
     def load_weights(self,load_from_path=False):
         if load_from_path:
@@ -66,13 +62,12 @@ class NN():
         else:
             attributes = self.training_set.shape[1]
             output_labels = len(self.processed_labels[0])
-            hidden_nodes1 = self.hidden_neurons
-            self.weights_hidden1 = np.random.rand(attributes, hidden_nodes1) * 0.01
-            self.bias_h1 = np.random.randn(hidden_nodes1)
-            self.weights_output = np.random.rand(hidden_nodes1, output_labels) * 0.01
+            self.weights_hidden1 = np.random.rand(attributes, self.hidden_neurons) * 0.01
+            self.bias_h1 = np.random.randn(self.hidden_neurons)
+            self.weights_output = np.random.rand(self.hidden_neurons, output_labels) * 0.01
             self.bias_o = np.random.randn(output_labels)
 
-    def save_weights(self,additional_path_in):
+    def save_weights(self):
         data = {
             'weights_hidden1': self.weights_hidden1.tolist(),
             'bias_h1': self.bias_h1.tolist(),
@@ -101,8 +96,8 @@ class NN():
     def training(self):
         self.load_data()
         self.data_preprocessing()
-        self.chunking()
         self.load_weights()
+
         error_cost = 0
 
         for i in range(self.desired_epochs):
@@ -112,41 +107,13 @@ class NN():
                 x_batch = self.training_set[step * self.batch_size:(step + 1) * self.batch_size]
                 y_batch = self.processed_labels[step * self.batch_size:(step + 1) * self.batch_size]
 
+                prediction_o,prediction_h1,X1,_ = self.forward(x_batch)
 
-                X1 = np.dot(x_batch, self.weights_hidden1) + self.bias_h1
-                prediction_h1 = self.sigmoid(X1)
-
-                y = np.dot(prediction_h1, self.weights_output) + self.bias_o
-                prediction_o = self.softmax(y)
-                
-                ######### Back Propagation
-
-                pred_h1 = prediction_h1
-                error_cost_o = prediction_o - y_batch
-                der_cost_o = np.dot(pred_h1.T, error_cost_o)
-                dcost_bo = error_cost_o
-
-
-                taining_data = x_batch
-
-                weight_o = self.weights_output
-                error_cost_h1 = np.dot(error_cost_o, weight_o.T)
-                derivative_h1 = self.sigmoid_der(X1)
-                pred_h2 = prediction_h1
-                der_cost_h1 = np.dot(taining_data.T, derivative_h1 * error_cost_h1)
-
-                dcost_bh1 = error_cost_h1 * derivative_h1
-
-                #hidden
-                self.weights_hidden1 -= self.learning_rate * der_cost_h1
-                self.bias_h1 -= self.learning_rate * dcost_bh1.sum(axis=0)
-                
-                #output
-                self.weights_output -= self.learning_rate * der_cost_o
-                self.bias_o -= self.learning_rate * dcost_bo.sum(axis=0)
+                self.backprop(prediction_o,prediction_h1,x_batch,y_batch,X1)
                 
                 loss = np.sum(-y_batch * np.log(prediction_o))
                 error_cost = loss
+
             print(error_cost)
 
             print('Iterations: ' + str(i))
@@ -159,26 +126,43 @@ class NN():
 
         y = np.dot(prediction_h1, self.weights_output) + self.bias_o
         prediction_o = self.softmax(y)
-        return prediction_o,prediction_h1
+        return prediction_o,prediction_h1,X1,y
+    
+    def backprop(self,prediction_o,prediction_h1,x_batch,y_batch,X1):
+        error_cost_o = prediction_o - y_batch
+        der_cost_o = np.dot(prediction_h1.T, error_cost_o)
+
+        weight_o = self.weights_output
+        error_cost_h1 = np.dot(error_cost_o, weight_o.T)
+
+        derivative_h1 = self.sigmoid_der(X1)
+        dcost_bh1 = error_cost_h1 * derivative_h1
+        der_cost_h1 = np.dot(x_batch.T, derivative_h1 * error_cost_h1)
+
+        #hidden
+        self.weights_hidden1 -= self.learning_rate * der_cost_h1
+        self.bias_h1 -= self.learning_rate * dcost_bh1.sum(axis=0)
+        
+        #output
+        self.weights_output -= self.learning_rate * der_cost_o
+        self.bias_o -= self.learning_rate * error_cost_o.sum(axis=0)
+
 
     def predict(self, sample):
-        output, _ = self.forward(sample)
+        output, _,_,_ = self.forward(sample)
         label = np.argmax(output)
         
         return label
         
     def testing(self):
         self.load_weights(load_from_path=True)
-        self.load_data()
-        self.data_preprocessing()
-        error = 0
-        correct = 0
-        count = 0
+        self.load_data(train=False)
+        self.data_preprocessing(train=False)
+        error,correct,count = 0,0,0
 
         for i in range(len(self.test_set)):
             result = self.predict(self.test_set[i])
             label = self.test_labels[i]
-            
             
             if (result == label):
                 print('SUCCESS,Result:',result,'Label',label)
@@ -194,11 +178,12 @@ class NN():
 
 
 if __name__ == "__main__":
-    epochs=100
+    epochs=125
     batch_size = 32
     amount_of_hidden_layer_neurons = 350
-    data_amount = 10000
-    path = 'training/params4.json'
+    data_amount = 15000
+    path = 'training/params1.json'
+    size_img = (28,28)
 
     net = NN(data_amount = data_amount,hidden_neurons=amount_of_hidden_layer_neurons,batch_size=batch_size,epochs=epochs,path=path)
     # net.load_data()
